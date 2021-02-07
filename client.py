177dda12a28c71b614d1c37c0f3cc03edb296fbc
jerrinjohn <78670462+jerrinjohn@users.noreply.github.com>
2021-02-07 11:07:01 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,37 @@
+#socket 
+#server started
+#client started
+#create connection 
+#sending message 
+#
+
+
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.50.17"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length)) # header 
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+fileHandle = open(r"C:\Users\JulianSaraJoseph\Downloads\messages.txt", 'r')
+
+for line in fileHandle:
+    send(line[:-2])
+fileHandle.close()
+
+send(DISCONNECT_MESSAGE)
